affb732d59bc27941f3f173e5c878086f4ccba2e
Tom0427 <82128211+Tom-game-project@users.noreply.github.com>
2024-03-18 17:12:23 +0900

    sep function

--- ex02/analog_clock.tcl
+++ ex02/analog_clock.tcl
@@ -1,6 +1,34 @@
 # Analog Clocks
 
+proc degree2position {a length resolution} {
+    set pi [expr 2.0 * asin(1.0)]
+    set step [expr 360 / $resolution]
+    set theta [expr $step * $a - 90]
+    return [list [expr 150 + $length * cos(2 * $pi * $theta / 360)] [expr 150 + $length * sin(2 * $pi * $theta / 360)]]
+}
 
+proc set_num {length} {
+    set pi [expr 2.0 * asin(1.0)]
+    for {set index 1} {$index <= 12} {incr index} {
+        set theta [expr 30 * $index - 90]
+        .c create text \
+        [degree2position $index 120 12]\
+        -anchor c -font Purisa -text $index 
+    }
+}
+
+
+proc show_time {} {
+    # show time
+    global buffer
+    set length 100
+    set buffer [clock format [clock seconds] -format "%H:%M:%S"]
+    set h [clock format [clock seconds] -format "%H"]
+    set m [clock format [clock seconds] -format "%M"]
+    set s [clock format [clock seconds] -format "%S"]
+    .c create line 150 150 150 150 -fill red -arrow last -width 2
+    after 1000 show_time
+}
 
 wm title . clock
 wm minsize . 200 50
@@ -8,18 +36,10 @@ wm minsize . 200 50
 # setting font
 option add *font {FixedSys 16}
 
-# setting label
-# set buffer "hello world"
-# label .l0 -textvariable buffer
-# pack .l0 -in .
-
 canvas .c -width 300 -height 300 -bg #9e9e9e
 pack .c -in .
 
-# .c create arc 20 20 280 280 -start 0 -extent 270 -fill yellow
-# .c create arc 20 20 280 280 -start 270 -extent 90 -fill red
-#.c create text 10 30 -anchor w -font Consolas -text "hello world"
-
 .c create oval 10 10 290 290 -fill pink
+set_num 120
 
-.c create line 150 150 280 150 -fill red -arrow last -width 3
+show_time
