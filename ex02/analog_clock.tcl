7b236e131f27cfc479eec03dca03d17db6b21949
Tom0427 <82128211+Tom-game-project@users.noreply.github.com>
2024-03-18 21:14:52 +0900

    add hour minute

--- ex02/analog_clock.tcl
+++ ex02/analog_clock.tcl
@@ -3,8 +3,11 @@
 proc degree2position {a length resolution} {
     set pi [expr 2.0 * asin(1.0)]
     set step [expr 360 / $resolution]
-    set theta [expr $step * {$a} - 90]
-    return [list [expr 150 + $length * cos(2 * $pi * $theta / 360)] [expr 150 + $length * sin(2 * $pi * $theta / 360)]]
+    set theta [expr $step * $a - 90]
+    return [list\
+        [expr 150 + $length * cos(2 * $pi * $theta / 360.0)]\
+        [expr 150 + $length * sin(2 * $pi * $theta / 360.0)]\
+    ]
 }
 
 proc set_num {length} {
@@ -20,13 +23,15 @@ proc set_num {length} {
 proc floop {} {
     global seconds_id
     global minutes_id
+    global hours_id
     set length 100
-    scan [clock format [clock seconds] -format "%I"] "%d" h
+    scan [clock format [clock seconds] -format "%H"] "%d" h
     scan [clock format [clock seconds] -format "%M"] "%d" m
     scan [clock format [clock seconds] -format "%S"] "%d" s
     set args [list 150 150]
-    .c coords $seconds_id [concat $args [degree2position $s 100 60]]
-    .c coords $minutes_id [concat $args [degree2position $m 100 60]]
+    .c coords $seconds_id [concat $args [degree2position $s 100 60.0]]
+    .c coords $minutes_id [concat $args [degree2position $m 100 60.0]]
+    .c coords $hours_id   [concat $args [degree2position [expr ($h % 12) * 60 + $m] 50 [expr 60.0 * 12.0]]]
     after 500 floop
 }
 
@@ -37,18 +42,17 @@ wm minsize . 200 50
 option add *font {FixedSys 16}
 
 canvas .c -width 300 -height 300 -bg #9e9e9e
-pack .c -in .
+pack   .c -in .
 
 .c create oval [list 10 10 290 290] -fill pink
 set_num 120
-
 
 scan [clock format [clock seconds] -format "%I"] "%d" h
 scan [clock format [clock seconds] -format "%M"] "%d" m
 scan [clock format [clock seconds] -format "%S"] "%d" s
 set args [list 150 150]
-set seconds_id [.c create line [concat $args [degree2position $s 100 60]] -fill red -arrow last -width 2]
-set minutes_id [.c create line [concat $args [degree2position $m 100 60]] -fill green -arrow last -width 5]
-#set hours_id   [.c create line [concat $args [degree2position $s 100 60]] -fill red -arrow last -width 5]
+set seconds_id [.c create line [concat $args [degree2position $s 100 60]] -fill red   -arrow last -width 2]
+set minutes_id [.c create line [concat $args [degree2position $m 100 60]] -fill black -arrow last -width 5]
+set hours_id   [.c create line [concat $args [degree2position $s 100 60]] -fill black -arrow last -width 5]
 
 floop
