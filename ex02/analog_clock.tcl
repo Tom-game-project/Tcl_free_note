8ff676be547d8f7d0da66598bc7a7e022aca42d6
Tom0427 <82128211+Tom-game-project@users.noreply.github.com>
2024-03-20 16:38:42 +0900

    edit size changed event proccess

--- ex02/analog_clock.tcl
+++ ex02/analog_clock.tcl
@@ -1,22 +1,46 @@
 # Analog Clocks
+# 
+# 
 
+
+# @param a
+# @param length
+# @param resolution
+# @returns position_array[2]
 proc degree2position {a length resolution} {
+    global width
+    set center_x [expr $width / 2]
+    set center_y [expr $width / 2]
     set pi [expr 2.0 * asin(1.0)]
     set step [expr 360 / $resolution]
     set theta [expr $step * $a - 90]
     return [list\
-        [expr 150 + $length * cos(2 * $pi * $theta / 360.0)]\
-        [expr 150 + $length * sin(2 * $pi * $theta / 360.0)]\
+        [expr $center_x + $length * cos(2 * $pi * $theta / 360.0)]\
+        [expr $center_y + $length * sin(2 * $pi * $theta / 360.0)]\
     ]
 }
 
 proc set_num {length} {
     set pi [expr 2.0 * asin(1.0)]
+    set nums_id_list []
     for {set index 1} {$index <= 12} {incr index} {
         set theta [expr 30 * $index - 90]
-        .c create text \
+        lappend nums_id_list [.c create text \
         [degree2position $index 120 12]\
-        -anchor c -font Purisa -text $index 
+        -anchor c -font Purisa -text $index ]
+    }
+    return $nums_id_list
+}
+
+
+# 
+proc draw_num {} {
+    global nums_id_list
+    set pi [expr 2.0 * asin(1.0)]
+    set index 1
+    foreach num_id $nums_id_list {
+        .c coords $num_id [degree2position $index 120 12]
+        incr index
     }
 }
 
@@ -34,12 +58,26 @@ proc draw_hand {x y h_length m_length s_length} {
 }
 
 proc floop {} {
-    draw_hand 150 150 50 100 100
+    global width
+    draw_hand \
+        [expr $width / 2.0]\
+        [expr $width / 2.0]\
+        [expr $width / 6.0]\
+        [expr $width / 3.0]\
+        [expr $width / 3.0]
     after 500 floop
 }
 
 proc window_size_changed {} {
-    puts "window size changed"
+    global width
+    set w [winfo width .c]
+    set h [winfo height .c]
+    if {$w > $h} {
+        set width $h
+    } else {
+        set width $w
+    }
+    draw_num 
 }
 
 wm title   . clock
@@ -51,20 +89,27 @@ option add *font {FixedSys 16}
 canvas .c -width 300 -height 300 -bg #9e9e9e
 pack   .c -in . -expand 1 -fill both
 
-
 # bind setting
 bind .c <Configure> window_size_changed
 
+set width 300
+
+
 .c create oval [list 10 10 290 290] -fill pink
-set_num 120
+# 1 - 12番までに割り当てられたidが格納される
+set nums_id_list [set_num 120]
+puts $nums_id_list 
+
 
 scan [clock format [clock seconds] -format "%I"] "%d" h
 scan [clock format [clock seconds] -format "%M"] "%d" m
 scan [clock format [clock seconds] -format "%S"] "%d" s
-set args [list 150 150]
+set args [list [expr $width / 2.0] [expr $width / 2.0]]
 set seconds_id [.c create line [concat $args [degree2position $s 100 60]] -fill red   -arrow last -width 2]
 set minutes_id [.c create line [concat $args [degree2position $m 100 60]] -fill black -arrow last -width 5]
 set hours_id   [.c create line [concat $args [degree2position $s 100 60]] -fill black -arrow last -width 5]
+
+# default clock width
 
 # start loop
 floop
